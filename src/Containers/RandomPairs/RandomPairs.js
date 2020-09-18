42cedd63261077b433cd2fd4492ca703a5fb10d9
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-09-18 17:09:28 +0530

    Convert RandomPairs into a Functional Component

--- src/Containers/RandomPairs/RandomPairs.js
+++ src/Containers/RandomPairs/RandomPairs.js
@@ -1,20 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Paper } from '@material-ui/core';
 import styles from './RandomPairs.module.css'
 import PlayGround from './PlayGround/PlayGround';
 
-class RandomPairs extends Component {
-
-    render() {
-        return (
-            <div className={styles.RandomPairs}>
-                <Paper className={styles.Paper} elevation={3}>
-                    <PlayGround />
-                </Paper>
-            </div>
-        )
-    }
-
+const RandomPairs = (props) => {
+    return (
+        <div className={styles.RandomPairs}>
+            <Paper className={styles.Paper} elevation={3}>
+                <PlayGround />
+            </Paper>
+        </div>
+    )
 }
 
 export default RandomPairs
