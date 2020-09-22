c9415d72fe5e7cc64ff32bee11ba078504123849
Akhil Krishnakumar <akhil.krishnakumar@springernature.com>
2020-09-22 17:23:35 +0530

    Create Pair model to store pair and feedback info

---
+++ src/Models/Pair.ts
@@ -0,0 +1,24 @@
+import { User } from "./User";
+
+export interface Pair { }
+
+export class SomePair implements Pair {
+
+  readonly user: User
+  readonly rating: number
+
+  constructor(
+    user: User,
+    rating: number = 3
+  ) {
+    this.user = user
+    this.rating = rating
+  }
+
+  name = () => this.user?.name ?? ""
+
+}
+
+export class NoPair implements Pair {
+  name = () => " "
+}
